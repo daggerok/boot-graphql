6b5f84df0938591a8b72173ac343b3164b126f2b
Maksim Kostromin <daggerok@gmail.com>
2017-11-14 04:37:48 +0200

    fix spring-boot/issues/10983

--- src/main/kotlin/daggerok/graphql/GraphQLConfig.kt
+++ src/main/kotlin/daggerok/graphql/GraphQLConfig.kt
@@ -5,22 +5,27 @@ import graphql.schema.idl.RuntimeWiring
 import graphql.schema.idl.SchemaGenerator
 import graphql.schema.idl.SchemaParser
 import graphql.schema.idl.TypeDefinitionRegistry
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+import java.io.InputStreamReader
 
 @Configuration
-class GraphQLConfig(val peopleDataFetcher: PeopleDataFetcher,
+class GraphQLConfig(val resources: ResourceLoader,
+                    val peopleDataFetcher: PeopleDataFetcher,
                     val personDataFetcher: PersonDataFetcher) {
-
-  @Value("classpath:/schema.graphqls")
-  lateinit var schemaResource: Resource
-
+/*
+  companion object {
+    private val log = org.slf4j.LoggerFactory.getLogger(GraphQLConfig::class.simpleName)
+  }
+*/
   @Bean
   fun registry(): TypeDefinitionRegistry {
-    val schemaFile = schemaResource.file
-    return SchemaParser().parse(schemaFile)
+    val resource = resources.getResource("classpath:schema.graphqls")
+
+    InputStreamReader(resource.inputStream).use {
+      return SchemaParser().parse(it.readText())
+    }
   }
 
   @Bean
